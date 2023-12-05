0db5b198be1266f034d07df46046aff73ca5373b
limhizy15 <limhizy15@ajou.ac.kr>
2023-12-05 16:09:54 +0900

    [#3] refactor: color 상수 파일 추가

---
+++ lib/components/Color/index.tsx
@@ -0,0 +1,19 @@
+import "./color.css";
+
+export const colors = {
+  black: "var(--black)",
+  white: "var(--white)",
+
+  gray900: "var(--gray-900)",
+  gray800: "var(--gray-800)",
+  gray700: "var(--gray-700)",
+  gray500: "var(--gray-500)",
+  gray400: "var(--gray-400)",
+  gray300: "var(--gray-300)",
+  gray200: "var(--gray-200)",
+
+  green500: "var(--green-500)",
+  blue500: "var(--blue-500)",
+  red500: "var(--red-500)",
+  purple300: "var(--purple-300)",
+};
