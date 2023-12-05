76a2c9eba169180e4d4f1fec921358fb32686e78
limhizy15 <limhizy15@ajou.ac.kr>
2023-12-05 14:36:51 +0900

    [#2] refactor: variants 객체 형태로 변경

--- lib/components/Typography/index.tsx
+++ lib/components/Typography/index.tsx
@@ -1,22 +1,22 @@
 import styles from "./styles.module.css";
 
-const variants = [
-  "caption2",
-  "caption2strong",
-  "caption1",
-  "caption1strong",
-  "body1",
-  "body1strong",
-  "body2",
-  "body2strong",
-  "subtitle1",
-  "title3",
-  "title2",
-  "title1",
-  "largetitle",
-] as const;
+const variants = {
+  caption2: "caption2",
+  caption2strong: "caption2strong",
+  caption1: "caption1",
+  caption1strong: "caption1strong",
+  body1: "body1",
+  body1strong: "body1strong",
+  body2: "body2",
+  body2strong: "body2strong",
+  subtitle1: "subtitle1",
+  title3: "title3",
+  title2: "title2",
+  title1: "title1",
+  largetitle: "largetitle",
+};
 
-type TypographyVariant = (typeof variants)[number];
+type TypographyVariant = keyof typeof variants;
 
 interface TypographyProps {
   text: string;
